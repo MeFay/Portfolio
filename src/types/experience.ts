bd11041f6e3a2aa1d33a41a6b37a348cea5fe37f
MeFay <fatimaalvesa10@gmail.com>
2024-08-21 18:54:28 +0100

    finished Skills Section and improved previous animation

--- src/types/experience.ts
+++ src/types/experience.ts
@@ -12,31 +12,52 @@ export const experience: ExperienceCategory[] = [
   {
     title: "UI/UX",
     data: [
-      { skill: "Figma", level: "Experienced" },
-      { skill: "Sketch", level: "Intermediate" },
-      { skill: "XD", level: "Intermediate" },
+      { skill: "Figma", level: "Intermediate" },
+      { skill: "Sketch", level: "Novice" },
+      { skill: "Canva", level: "Intermediate" },
     ],
   },
   {
-    title: "Frontend Development",
+    title: "JavaScript Frameworks & Libraries",
     data: [
-      { skill: "HTML", level: "Experienced" },
-      { skill: "CSS", level: "Experienced" },
-      { skill: "JavaScript", level: "Experienced" },
-      { skill: "Tailwind", level: "Intermediate" },
-      { skill: "Bootstrap", level: "Experienced" },
-      { skill: "React", level: "Experienced" },
-      { skill: "React Native", level: "Experienced" },
+      { skill: "React.js", level: "Proficient" },
+      { skill: "Redux.js", level: "Intermediate" },
+      { skill: "JavaScript", level: "Proficient" },
+      { skill: "TypeScript", level: "Intermediate" },
+    ],
+  },
+  {
+    title: "CSS Frameworks & Preprocessors",
+    data: [
+      { skill: "CSS", level: "Proficient" },
+      { skill: "SASS", level: "Intermediate" },
+      { skill: "styled-components", level: "Intermediate" },
+      { skill: "Tailwind CSS", level: "Intermediate" },
+      { skill: "Bootstrap", level: "Intermediate" },
+    ],
+  },
+  {
+    title: "Responsive Design & Cross-Browser",
+    data: [
+      { skill: "Responsive Web Design", level: "Proficient" },
+      { skill: "Cross-Browser Development", level: "Intermediate" },
+    ],
+  },
+  {
+    title: "APIs & Debugging",
+    data: [
+      { skill: "REST APIs", level: "Proficient" },
+      { skill: "Browser Developer Tools", level: "Intermediate" },
     ],
   },
   {
     title: "Backend Development",
     data: [
-      { skill: "Node JS", level: "Experienced" },
-      { skill: "MongoDB", level: "Intermediate" },
-      { skill: "PHP", level: "Experienced" },
-      { skill: "Python", level: "Intermediate" },
-      { skill: "MySQL", level: "Experienced" },
+      { skill: "Node.js", level: "Novice" },
+      { skill: "MongoDB", level: "Novice" },
+      { skill: "Java", level: "Intermediate" },
+      { skill: "Spring Boot", level: "Novice" },
+      { skill: "GraphQL", level: "Novice" },
     ],
   },
 ];
